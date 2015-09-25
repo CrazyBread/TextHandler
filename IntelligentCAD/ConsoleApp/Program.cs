6a2a7e7456e36e5d51b27b2cc5211c196f6be9fb
Alexeyyy <a.zhelepov@yandex.ru>
2015-09-25 22:46:14 +0300

    TF anf TF_IDF were implemented as well as word frequency analyzer

--- IntelligentCAD/ConsoleApp/Program.cs
+++ IntelligentCAD/ConsoleApp/Program.cs
@@ -6,10 +6,24 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            TextHandler handler = new TextHandler();
-            string text = handler.ConvertTextToString("text.txt");
-            var words = handler.GetWords(text, "[^а-яА-Я]+");
-            var dictionary = handler.GetWordsCount(words);
+            TextHandler handler = new TextHandler("prepositions.txt", "unions.txt");
+            var wordsList = handler.CutOutWords("text.txt", "[^a-zA-Zа-яА-ЯёЁ0-9-]+");
+
+            //1. Частотный словарь
+            FrequencyDictionary fd = new FrequencyDictionary();
+            var dictionary = fd.GetWordsCount(wordsList);
+            int wordsCount = fd.GetWordsCount(dictionary);
+            //Чистим от предлогов/союзов
+            dictionary = handler.ApplyExclusionMask(handler.Prepositions, dictionary);
+            dictionary = handler.ApplyExclusionMask(handler.Unions, dictionary);
+            handler.PrintWordsTop(dictionary, 20, "Частотный словарь");
+
+            //2.TF_IDF
+            TF_IDF ti = new TF_IDF();
+            var tf_dictionary = ti.GetTF(dictionary, wordsCount);
+            handler.PrintWordsTop(tf_dictionary, 20, "TF (абсолютная частота встречаемости)");
+            var tfidf_dictionary = ti.GetTF_IDF(100000, tf_dictionary);
+            handler.PrintWordsTop(tfidf_dictionary, 20, "TF*IDF(обратная частота документа)");
             Console.WriteLine("Done!");
         }
     }
--- IntelligentCAD/ConsoleApp/TextHandler.cs
+++ IntelligentCAD/ConsoleApp/TextHandler.cs
@@ -2,13 +2,58 @@
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.IO;
-using System.Text;
+using System;
 
 namespace ConsoleApp
 {
+    /// <summary>
+    /// Статистический метод TF_IDF
+    /// </summary>
+    public class TF_IDF 
+    {
+        public Dictionary<string, double> GetTF_IDF(int ccSize, Dictionary<string, double> tf_dictionary, int docNumber = 1) 
+        {
+            return tf_dictionary.ToDictionary(i => i.Key, i => i.Value * Math.Log((double)(ccSize - docNumber) / docNumber));
+        }
+
+        public Dictionary<string, double> GetTF(Dictionary<string, int> words, int count) 
+        {
+            return words.ToDictionary(i => i.Key, i => ((double)i.Value / count));
+        }
+    }
+
+    public class FrequencyDictionary 
+    {
+        public Dictionary<string, int> GetWordsCount(List<string> words)
+        {
+            return
+                (
+                    from word in words
+                    group words by word into grp
+                    select new { word = grp.Key, count = grp.Count() }
+                )
+                .ToDictionary(i => i.word, i => i.count);
+        }
+
+        public int GetWordsCount(Dictionary<string, int> dictionary)  
+        {
+            return dictionary.Sum(i => i.Value);
+        } 
+    }
+
     public class TextHandler
     {
-        public TextHandler() { }
+        private List<string> prepositions; //предлоги
+        private List<string> unions;
+
+        public List<string> Prepositions { get { return prepositions; } }
+        public List<string> Unions { get { return unions; } }
+
+        public TextHandler(string prepositionsFilePath, string unionsFilePath) 
+        {
+            prepositions = LoadLinesFromFile(prepositionsFilePath);
+            unions = LoadLinesFromFile(unionsFilePath);
+        }
 
         public List<string> GetWords(string text, string pattern) 
         {
@@ -17,30 +62,56 @@ namespace ConsoleApp
                 .Select(i => i.ToLower())
                 .ToList();
         }
-        
-        public Dictionary<string, int> GetWordsCount(List<string> words) 
+
+        public Dictionary<string, int> ApplyExclusionMask(List<string> maskWords, Dictionary<string, int> words) 
         {
-           return 
-               (
-                   from word in words
-                   group words by word into grp
-                   select new { word = grp.Key, count = grp.Count() }
-               )
-               .ToDictionary(i => i.word, i => i.count);
+            return words.Where(i => !maskWords.Contains(i.Key)).ToDictionary(i => i.Key, i => i.Value);
         }
 
         //Временно
         //http://habrahabr.ru/post/147927/
-        public string ConvertTextToString(string filePath) 
+        public List<string> CutOutWords(string filePath, string pattern) 
         {
             Stream stream = new FileStream(filePath, FileMode.Open);
             StreamReader reader = new StreamReader(stream);
-            StringBuilder text = new StringBuilder();
+            string str = string.Empty;
+            List<string> words = new List<string>();
+
             while (!reader.EndOfStream) 
             {
-                text.AppendLine(reader.ReadLine());
+                str = reader.ReadLine();
+                words.AddRange(GetWords(str, pattern));
             }
-            return text.ToString();
+
+            return words;
+        }
+
+        private List<string> LoadLinesFromFile(string filePath) 
+        {
+            if (!File.Exists(filePath))
+                return null;
+            
+            List<string> list = new List<string>();
+
+            Stream stream = new FileStream(filePath, FileMode.Open);
+            StreamReader reader = new StreamReader(stream);
+
+            while (!reader.EndOfStream) 
+            {
+                list.Add(reader.ReadLine());
+            }
+
+            return list;
+        }
+
+        public void PrintWordsTop<T>(Dictionary<string, T> dictionary, int topCount, string phrase)
+        {
+            Console.WriteLine("==============={0}===============", phrase);
+            var top = dictionary.OrderByDescending(i => i.Value).Take(topCount);
+            foreach (var item in top)
+            {
+                Console.WriteLine("{0} - {1}", item.Key, item.Value);
+            }
         }
     }
 }
