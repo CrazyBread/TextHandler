0154bd823d05443eac172cf2e518d0d6e427b537
Qui-Gon173 <quigon173@gmail.com>
2015-10-09 20:39:54 +0300

    Some code cleaning

--- IntelligentCAD/ConsoleApp/Program.cs
+++ IntelligentCAD/ConsoleApp/Program.cs
@@ -6,24 +6,24 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            TextHandler handler = new TextHandler("prepositions.txt", "unions.txt");
-            var wordsList = handler.CutOutWords("text.txt", "[^a-zA-Zа-яА-ЯёЁ0-9-]+");
+            var prepositions = TextHandler.LoadLinesFromFile("prepositions.txt");
+            var unions = TextHandler.LoadLinesFromFile("unions.txt");
+            var wordsList = TextHandler.CutOutWords("text.txt", "[^a-zA-Zа-яА-ЯёЁ0-9-]+");
 
             //1. Частотный словарь
-            FrequencyDictionary fd = new FrequencyDictionary();
-            var dictionary = fd.GetWordsCount(wordsList);
-            int wordsCount = fd.GetWordsCount(dictionary);
+            var dictionary = FrequencyDictionary.GetFrequenceDictionary(wordsList);
+            int wordsCount = FrequencyDictionary.GetWordsCount(wordsList);
+
             //Чистим от предлогов/союзов
-            dictionary = handler.ApplyExclusionMask(handler.Prepositions, dictionary);
-            dictionary = handler.ApplyExclusionMask(handler.Unions, dictionary);
-            handler.PrintWordsTop(dictionary, 20, "Частотный словарь");
+            dictionary = TextHandler.ApplyExclusionMask(prepositions, dictionary);
+            dictionary = TextHandler.ApplyExclusionMask(unions, dictionary);
+            TextHandler.PrintWordsTop(dictionary, 20, "Частотный словарь");
 
             //2.TF_IDF
-            TF_IDF ti = new TF_IDF();
-            var tf_dictionary = ti.GetTF(dictionary, wordsCount);
-            handler.PrintWordsTop(tf_dictionary, 20, "TF (абсолютная частота встречаемости)");
-            var tfidf_dictionary = ti.GetTF_IDF(100000, tf_dictionary);
-            handler.PrintWordsTop(tfidf_dictionary, 20, "TF*IDF(обратная частота документа)");
+            var tf_dictionary = TF_IDF.GetTF(dictionary, wordsCount);
+            TextHandler.PrintWordsTop(tf_dictionary, 20, "TF (абсолютная частота встречаемости)");
+            var tfidf_dictionary = TF_IDF.GetTF_IDF(100000, tf_dictionary);
+            TextHandler.PrintWordsTop(tfidf_dictionary, 20, "TF*IDF(обратная частота документа)");
             Console.WriteLine("Done!");
         }
     }
--- IntelligentCAD/ConsoleApp/TextHandler.cs
+++ IntelligentCAD/ConsoleApp/TextHandler.cs
@@ -9,22 +9,22 @@ namespace ConsoleApp
     /// <summary>
     /// Статистический метод TF_IDF
     /// </summary>
-    public class TF_IDF 
+    public static class TF_IDF
     {
-        public Dictionary<string, double> GetTF_IDF(int ccSize, Dictionary<string, double> tf_dictionary, int docNumber = 1) 
+        public static Dictionary<string, double> GetTF_IDF(int ccSize, Dictionary<string, double> tf_dictionary, int docNumber = 1)
         {
             return tf_dictionary.ToDictionary(i => i.Key, i => i.Value * Math.Log((double)(ccSize - docNumber) / docNumber));
         }
 
-        public Dictionary<string, double> GetTF(Dictionary<string, int> words, int count) 
+        public static Dictionary<string, double> GetTF(Dictionary<string, int> words, int count)
         {
             return words.ToDictionary(i => i.Key, i => ((double)i.Value / count));
         }
     }
 
-    public class FrequencyDictionary 
+    public static class FrequencyDictionary
     {
-        public Dictionary<string, int> GetWordsCount(List<string> words)
+        public static Dictionary<string, int> GetFrequenceDictionary(List<string> words)
         {
             return
                 (
@@ -35,27 +35,15 @@ namespace ConsoleApp
                 .ToDictionary(i => i.word, i => i.count);
         }
 
-        public int GetWordsCount(Dictionary<string, int> dictionary)  
+        public static int GetWordsCount(List<string> wordList)
         {
-            return dictionary.Sum(i => i.Value);
-        } 
+            return wordList.Count;
+        }
     }
 
-    public class TextHandler
+    public static class TextHandler
     {
-        private List<string> prepositions; //предлоги
-        private List<string> unions;
-
-        public List<string> Prepositions { get { return prepositions; } }
-        public List<string> Unions { get { return unions; } }
-
-        public TextHandler(string prepositionsFilePath, string unionsFilePath) 
-        {
-            prepositions = LoadLinesFromFile(prepositionsFilePath);
-            unions = LoadLinesFromFile(unionsFilePath);
-        }
-
-        public List<string> GetWords(string text, string pattern) 
+        public static List<string> GetWords(string text, string pattern)
         {
             return Regex.Split(text, pattern)
                 .Where(i => !string.IsNullOrEmpty(i))
@@ -63,48 +51,46 @@ namespace ConsoleApp
                 .ToList();
         }
 
-        public Dictionary<string, int> ApplyExclusionMask(List<string> maskWords, Dictionary<string, int> words) 
+        public static Dictionary<string, int> ApplyExclusionMask(List<string> maskWords, Dictionary<string, int> words)
         {
             return words.Where(i => !maskWords.Contains(i.Key)).ToDictionary(i => i.Key, i => i.Value);
         }
 
         //Временно
         //http://habrahabr.ru/post/147927/
-        public List<string> CutOutWords(string filePath, string pattern) 
+        public static List<string> CutOutWords(string filePath, string pattern)
         {
-            Stream stream = new FileStream(filePath, FileMode.Open);
-            StreamReader reader = new StreamReader(stream);
             string str = string.Empty;
             List<string> words = new List<string>();
-
-            while (!reader.EndOfStream) 
-            {
-                str = reader.ReadLine();
-                words.AddRange(GetWords(str, pattern));
-            }
+            using (var reader = new StreamReader(filePath))
+                while (!reader.EndOfStream)
+                {
+                    str = reader.ReadLine();
+                    words.AddRange(GetWords(str, pattern));
+                }
 
             return words;
         }
 
-        private List<string> LoadLinesFromFile(string filePath) 
+        public static List<string> LoadLinesFromFile(string filePath)
         {
             if (!File.Exists(filePath))
                 return null;
-            
-            List<string> list = new List<string>();
 
-            Stream stream = new FileStream(filePath, FileMode.Open);
-            StreamReader reader = new StreamReader(stream);
+            var list = new List<string>();
 
-            while (!reader.EndOfStream) 
+            using (var reader = new StreamReader(filePath))
             {
-                list.Add(reader.ReadLine());
+                while (!reader.EndOfStream)
+                {
+                    list.Add(reader.ReadLine());
+                }
             }
 
             return list;
         }
 
-        public void PrintWordsTop<T>(Dictionary<string, T> dictionary, int topCount, string phrase)
+        public static void PrintWordsTop<T>(Dictionary<string, T> dictionary, int topCount, string phrase)
         {
             Console.WriteLine("==============={0}===============", phrase);
             var top = dictionary.OrderByDescending(i => i.Value).Take(topCount);
